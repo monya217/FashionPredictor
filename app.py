ba79491161b0d24fd52766869f207ff8c83ec82a
Monya Mehta <122296853+monya217@users.noreply.github.com>
2025-08-29 16:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import uvicorn
+from io import BytesIO
 
 # Load trained model
 model = load_model("fashion_cnn_model.keras")
@@ -24,16 +25,24 @@ def home():
     </form>
     """)
 
+from io import BytesIO
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    img = image.load_img(file.file, target_size=(28,28), color_mode="grayscale")
+    contents = await file.read()   # read file bytes
+    img = image.load_img(BytesIO(contents), target_size=(28,28), color_mode="grayscale")
+    
+    # Convert to array + preprocess
     img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
-    pred = model.predict(img_array)
+    # Predict
+    pred = model.predict(img_array, verbose=0)
     pred_class = np.argmax(pred, axis=1)[0]
+    
     return {"filename": file.filename, "prediction": class_names[pred_class]}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
